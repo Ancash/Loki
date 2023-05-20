7e8e01674565aaf55f7cd53d4d89a72ec0202b62
Ancash <bdspark2005@gmail.com>
2023-05-20 11:37:41 +0200

    .

--- src/de/ancash/loki/LokiPluginDescription.java
+++ src/de/ancash/loki/LokiPluginDescription.java
@@ -9,7 +9,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import de.ancash.libs.org.simpleyaml.configuration.file.YamlFile;
+import org.simpleyaml.configuration.file.YamlFile;
 
 public class LokiPluginDescription {
 
